7b52dd3593638460bfef96ff98b563f0d9b30704
Brett Cassedy <brett.cassedy@gmail.com>
2014-04-10 13:18:35 -0700

    Finish board class. Add game class

---
+++ board.rb
@@ -0,0 +1,115 @@
+require 'colorize'
+require 'debugger'
+
+class InvalidMoveError < StandardError
+end
+
+class Board
+  def initialize(board = nil)
+    @board = board || Array.new(8) { Array.new(8) }
+  end
+
+  def [](pos)
+    row, col = pos
+    @board[row][col]
+  end
+
+  def []=(pos, piece)
+    row, col = pos
+    @board[row][col] = piece
+  end
+
+  def add_piece(piece, pos)
+    self[pos] = piece
+  end
+
+  def valid_move_seq?(moves)
+    begin
+      start_pos = moves[0][0]
+      board_copy = copy_game_state(start_pos)
+      board_copy.perform_moves!(moves)
+    rescue InvalidMoveError
+      return false
+    end
+    true
+  end
+
+  def perform_moves!(moves)
+    # need to finish this method
+    until moves.empty? do
+      start_pos, end_pos = moves.shift
+      move(start_pos, end_pos)
+    end
+  end
+
+  def copy_game_state(start_pos)
+    #Dup the board and the relevant piece
+    board_copy = Board.new(dup)
+    piece = board_copy[start_pos]
+    new_piece = Piece.new(board_copy, piece.color, piece.pos)
+    board_copy
+  end
+
+
+  def dup
+    copy = []
+    @board.each do |row|
+      copy << row.dup
+    end
+    copy
+  end
+
+
+
+  def move(start_pos, end_pos)
+    piece = self[start_pos]
+    if piece.perform_slide(end_pos) || piece.perform_jump(end_pos)
+      self[start_pos] = nil
+      self[end_pos] = piece
+      piece.pos = end_pos
+      piece.maybe_promote
+    else
+      raise InvalidMoveError
+    end
+  end
+
+  def empty?(pos)
+    self[pos].nil?
+  end
+
+  def setup_board
+    [0,1,2,5,6,7].each do |row_num|
+      row_num < 4 ? color = :black : color = :red
+      row_num.even? ? to_fill = :odd? : to_fill = :even?
+      (0..7).each do |col_num|
+        pos = [row_num, col_num]
+        self[pos] = Piece.new(self, color, pos) if col_num.send(to_fill)
+      end
+    end
+    nil
+  end
+
+  def to_s
+    next_color = { :green => :white, :white => :green }
+    output = "   a b c d e f g h\n "
+    cur_color = :green
+    @board.each_with_index do |row, row_index|
+      cur_color = next_color[cur_color]
+      output << "#{8 - row_index} "
+      row.each do |space|
+        if space.nil?
+          output << "  ".colorize(:background => cur_color)
+        else
+          output << space.display_char.colorize(:color => space.color,
+          :background => cur_color)
+        end
+        cur_color = next_color[cur_color]
+      end
+      output << "#{8 - row_index}"
+      output << "\n "
+    end
+    output << "  a b c d e f g h\n "
+    output
+  end
+
+end
--- piece.rb
+++ piece.rb
@@ -1,19 +1,108 @@
+require 'debugger'
+
 class Piece
   attr_accessor :pos
-  attr_reader :color
+  attr_reader :color, :board
 
-  def initialize(color, pos)
+  SLIDES = {
+    :black => { :soldier => [ [1, 1], [1, -1] ],
+                :king => [ [1, 1], [1, -1], [-1, 1], [-1, -1] ]
+              },
+    :red => { :soldier => [ [-1, 1], [-1, -1] ],
+              :king => [ [1, 1], [1, -1], [-1, 1], [-1, -1] ]
+            }
+  }
+
+  JUMPS = {
+    :black => { :soldier => [ [2, 2], [2, -2] ],
+                :king => [ [2, 2], [2, -2], [-2, 2], [-2, -2] ]
+              },
+    :red => { :soldier => [ [-2, 2], [-2, -2] ],
+              :king => [ [2, 2], [2, -2], [-2, 2], [-2, -2] ]
+            }
+  }
+
+
+  def initialize(board, color, pos)
+    @board = board
     @color = color
     @pos = pos
     @king = false
+    @board.add_piece(self, pos)
+  end
+
+  def type
+    king? ? :king : :soldier
+  end
+
+  def slides
+    SLIDES[color][type]
+  end
+
+  def promote_row
+    black? ? 7 : 0
+  end
+
+  def black?
+    color == :black
+  end
+
+  def white?
+    color == :white
+  end
+
+  def perform_slide(pos)
+    @board[pos].nil? && move_difs.include?(pos_dif(pos))
+  end
+
+  def perform_jump(pos)
+    return false unless valid_jump?(pos)
+    jumped_space = jumped_space(self.pos, pos)
+    unless @board.empty?(jumped_space) || 
+                                @board[jumped_space].color == self.color
+      @board[jumped_space] = nil
+      true
+    else
+      false
+    end
+  end
+
+  def valid_jump?(pos)
+    difference = pos_dif(pos)
+    @board.empty?(pos) && possible_jumps.include?(difference)
+  end
+
+  def jumped_space(start_pos, end_pos)
+    dif_to_jumped_piece = pos_dif(end_pos).map { |coord| coord / 2 }
+    [start_pos[0] + dif_to_jumped_piece[0], start_pos[1] + dif_to_jumped_piece[1]]
+  end
+
+  def possible_jumps
+    JUMPS[self.color][self.type]
+  end
+
+  def move_difs
+    SLIDES[self.color][self.type]
+  end
+
+  def pos_dif(pos)
+    #returns the difference between the start position and end position
+    [pos[0] - self.pos[0], pos[1] - self.pos[1]]
   end
 
   def king?
     @king
   end
 
+  def maybe_promote
+    promote if self.pos[0] == promote_row
+  end
+
   def promote
     @king = true
   end
-  
+
+  def display_char
+    'O '
+  end
 end
